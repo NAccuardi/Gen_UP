452cedff3ddbe1da7e5f00f45179e8c4d1db2034
Melanie Martinell <melaniemartinell@Melanies-MacBook-Pro.local>
2017-09-29 23:07:19 -0700

    first commit

---
+++ seniorDesign/Controllers/AddController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTecBits_Bootstrap_Empty_MVC.Controllers
+{
+    public class AddController : Controller
+    {
+        public ActionResult Add()
+        {
+            return View ();
+        }
+    }
+}
