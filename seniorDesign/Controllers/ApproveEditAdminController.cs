e64a422374c8bcbafd6f2de0b50b1c6d384badbf
martinme18 <martinme18@up.edu>
2018-02-28 22:18:47 -0800

    allows users to request edit, admin can approve/delete

---
+++ seniorDesign/Controllers/ApproveEditAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace seniorDesign.Controllers
+{
+    public class ApproveEditAdminController : Controller
+    {
+        // GET: ApproveEditAdmin
+        public ActionResult ApproveEditAdmin()
+        {
+            return View();
+        }
+    }
+}
---
+++ seniorDesign/Controllers/Edit_Approval_Table.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MyTecBits_Bootstrap_Empty_MVC.Controllers
+{
+    public class Edit_Approval_Table : Controller
+    {
+        public ActionResult EditTable()
+        {
+            return View();
+        }
+    }
+}
